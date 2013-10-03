e9554a7b6b6b5e2eaf09484241b7f181fccaae60
William Kenyon <github@abacathoo.org>
2013-10-03 09:21:18 +0000

    Add the Tracepoint type (to BacktraceTypes.hs). A tracepoint is very much like a SCC in profiling. It holds the name, module and location of the function.

--- compiler/backtrace/BacktraceTypes.hs
+++ compiler/backtrace/BacktraceTypes.hs
@@ -3,4 +3,39 @@
 --                                              --
 -- (c) William Kenyon 2013                      --
 --------------------------------------------------
-module BacktraceTypes where
+
+module BacktraceTypes(Tracepoint(..)) where
+import Outputable
+import Module
+import SrcLoc
+import FastString
+import Unique
+import Data.Data
+import Util
+
+data Tracepoint =  Tracepoint { tp_key  :: Int
+                              , tp_name :: FastString
+                              , tp_mod  :: Module
+                              , tp_loc  :: SrcSpan }
+  deriving (Data, Typeable)
+
+instance Eq Tracepoint where
+  tp1 == tp2 = isEqual $ compare tp1 tp2
+
+instance Ord Tracepoint where
+  tp1 `compare` tp2 = thenCmp (tp_mod tp1 `compare` tp_mod tp2)
+                              (tp_key tp1 `compare` tp_key tp2)
+
+
+instance Outputable Tracepoint where
+  ppr tp = getPprStyle $ \ sty ->
+    if codeStyle sty
+      then pprTracepointLbl tp
+      else text $ unpackFS $ tp_name tp
+
+pprTracepointLbl :: Tracepoint -> SDoc
+pprTracepointLbl (Tracepoint{ tp_key  = k
+                            , tp_name = n
+                            , tp_mod = m })
+  = ppr m <> char '_' <> ztext (zEncodeFS n) <> char '_'
+          <> ppr (mkUniqueGrimily k) <> text "_tracepoint"
